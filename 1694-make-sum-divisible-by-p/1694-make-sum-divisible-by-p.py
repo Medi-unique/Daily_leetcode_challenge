7536146a566005b19aa9976708ee6221ec438ce8
Medina Nesro <mediunique00@gmail.com>
2024-10-03 08:14:48 +0300

    Time: 420 ms (17.24%), Space: 40.6 MB (5.06%) - LeetHub

--- 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.py
+++ 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.py
@@ -1,26 +1,22 @@
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
-        
-        total_sum = sum(nums)
-        target_rem = total_sum % p
-        
-        if target_rem == 0:
-            return 0 
-            
-        prefix_sum = 0
-        prefix_sums = {0: -1}  
-        min_length = len(nums)
-        curr_rem = 0
-        
-        for i, num in enumerate(nums):
-            prefix_sum += num
-            curr_rem = prefix_sum % p
-            prev_rem = (curr_rem - target_rem + p) % p  
-            
-            if prev_rem in prefix_sums:
-                min_length = min(min_length, i - prefix_sums[prev_rem])
-            
-            prefix_sums[curr_rem] = i
-        
-        return min_length if min_length < len(nums) else -1  
+        total = sum(nums)
+        if total % p == 0:
+            return 0
 
+        cur_sum = 0
+        prefix_sum = []
+        for i in range(len(nums)):
+            cur_sum += nums[i]
+            prefix_sum.append(cur_sum)
+
+        tracker = {
+            0: -1
+        }
+        res = float('inf')
+        for j in range(len(prefix_sum)):
+            if ((prefix_sum[j] % p) - (total % p)) % p in tracker:
+                res = min(res, j - tracker[((prefix_sum[j] % p) - (total % p)) % p])
+            tracker[prefix_sum[j] % p] = j
+
+        return res if res != len(nums) else -1
