ce0d7a5fe51e41c2a42aad023b1782f6a79c91ad
Medina Nesro <mediunique00@gmail.com>
2025-04-20 18:35:37 +0300

    Time: 0 ms (100%), Space: 18.1 MB (29.1%) - LeetHub

---
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        count=Counter(answers)
+        res=0
+        for key,value in count.items():
+            if key==0:
+                res += value
+
+            elif value<= key+1:
+                res+=key+1
+            else:
+                res+= (ceil(value/(key+1)) * (key+1))
+        return res
+
+
+        
